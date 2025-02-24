df8fd565073fa0637ebb92bd34c29f056f23c7c6
Sudip Shrestha <85349701+SUDIP10K89@users.noreply.github.com>
2025-02-24 15:20:34 +0545

    Update SubjectDetailsPage.jsx

--- client/src/pages/SubjectDetailsPage.jsx
+++ client/src/pages/SubjectDetailsPage.jsx
@@ -29,7 +29,7 @@ const SubjectDetailsPage = () => {
   ];
 
   return (
-    <div className="container mx-auto px-4 py-8 pt-20">
+    <div className="container mx-auto px-4 py-8 pt-25">
       <div className="bg-gradient-to-r from-blue-700 to-purple-700 text-white p-8 rounded-lg mb-8">
         <h1 className="text-3xl font-bold text-center">
           {subjectName}
