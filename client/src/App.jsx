388887062c8ecafe24ca0158dc42148ca70672c1
SUDIP10K89 <sudip10k89@gmail.com>
2025-02-26 20:57:23 +0545

    Add PWA support with vite-plugin-pwa and update manifest configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,19 +9,6 @@ import SlidesNotes from "./pages/SlidesNotes";
 
 
 const App = () => {
-  //Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-      .then(() => {
-        console.log('ServiceWorker registration successful');
-      })
-      .catch((err) => {
-
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
   return (
     <BrowserRouter>
         <Routes>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/", 
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Learn Computer Engineering",
+        short_name: "LearnCE",
+        description: "Awesome app for learning Computer Engineering",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/assets/img/learnceLogo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/learnceLogo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg}"],
+      },
+    }),
+  ],
+  base: "/",
 });
+
