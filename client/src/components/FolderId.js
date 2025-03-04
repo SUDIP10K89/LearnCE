0aa7e99fd3bd7b773699139d2cfbab10a080189b
Sudip Shrestha <85349701+SUDIP10K89@users.noreply.github.com>
2025-03-04 10:22:15 +0545

    Update FolderId.js

--- client/src/components/FolderId.js
+++ client/src/components/FolderId.js
@@ -6,8 +6,8 @@ export const FolderId = [
           name: "Engineering Mathematics I",
           categories: {
             questions: "1htkp4xD16-HrMflsur1P-sTAXe4gffic",
-            books: "jhasdjkahsdhjka",
-            notes: "jhghadjkhskjhd",
+            books: "11JwksH_FzX_ev1e9IbDF3BjZYKrHsK1x",
+            notes: "11FdwWyMmCkRKu0TrJDgarvCtfwW2KcAV",
           },
         },
         {
@@ -21,33 +21,33 @@ export const FolderId = [
         {
           name: "Engineering Drawing",
           categories: {
-            questions: "sdhshdsdhjjkds",
-            books: "jhasdjkahsdhjka",
-            notes: "jhghadjkhskjhd",
+            questions: "11LEX9vgKU-epezzMowNwG8wOzCtFCBZu",
+            books: "11MW5vMUp0fJ8vvjYvmN6dsY_YdGNWCn7",
+            notes: "11LRwrZnAPri3AtwRjHSGdTxG9BJZCYHW",
           },
         },
         {
           name: "Fundamentals of Electrical and Electronics Engineering",
           categories: {
-            questions: "sdhshdsdhjjkds",
-            books: "jhasdjkahsdhjka",
-            notes: "jhghadjkhskjhd",
+            questions: "1178m1lXiZ84i0hLGeVA6nNfzxGAtO2pN",
+            books: "110FiLLSWZZtDRcH77wFzXeJrz6gAN8Tx",
+            notes: "119PioWnBPVZlHZKbHJFUDvznz_-cd_vl",
           },
         },
         {
           name: "Engineering Physics",
           categories: {
-            questions: "sdhshdsdhjjkds",
-            books: "jhasdjkahsdhjka",
-            notes: "jhghadjkhskjhd",
+            questions: "11T32rnr7cjJho2YuKp3iE-CXr7aWlXJ8",
+            books: "11UB43H9U4mfLAc58eFme9sGRtizxnFU8",
+            notes: "11V3DJYA1FmzNGyVIDMyP55tdKciU_wm-",
           },
         },
         {
           name: "Engineering Workshop",
           categories: {
-            questions: "sdhshdsdhjjkds",
-            books: "jhasdjkahsdhjka",
-            notes: "jhghadjkhskjhd",
+            questions: "11VdoGYUKRlJevPL_zTRjcgH8cw6FMpoo",
+            books: "11Ym9TN1CzHsoUSr5UNqnAnN3OPUjt2MW",
+            notes: "11Zzr5emxzf3uBOBBJZaeEuc42WiLTiha",
           },
         },
       ],
