c0be76c9658f9635d68a05bace8398c38490bfcd
Sudip Shrestha <sudip10k89@gmail.com>
2025-03-05 19:53:31 +0545

    Update service worker cache revision for index.html

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-417a7fbf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gguoch0t7ho"
+    "revision": "0.8dvccjvq318"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
