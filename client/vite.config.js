f8330285c35fa867332c145b1dc623d62452e493
Sudip Shrestha <85349701+SUDIP10K89@users.noreply.github.com>
2025-03-14 19:47:40 +0545

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "/assets/img/learnceLogo.png",
+            src: "/assets/img/BlackLogo.png",
             sizes: "192x192",
             type: "image/png",
           },
